b6972b72dd482ef2eb1ee67d289f67c48028d9dc
Asna A <asna.a@geekywolf.local>
2024-10-30 14:06:30 +0530

    login without conn

---
+++ AppointmentScheduling.API/Controllers/CheckDoctorExistController.cs
@@ -0,0 +1,25 @@
+﻿using AppointmentScheduling.Application.Request.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppointmentScheduling.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CheckDoctorExistController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public CheckDoctorExistController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<string> checkUsernamePassword(CheckDoctorExist checkDoctorExist)
+        {
+            return await mediator.Send(checkDoctorExist);
+        }
+    }
+}
---
+++ AppointmentScheduling.Application/Request/Commands/CheckPatientExist.cs
@@ -0,0 +1,34 @@
+﻿using AppointmentScheduling.Infrastructure.Data;
+using MediatR;
+
+namespace AppointmentScheduling.Application.Request.Commands
+{
+    public class CheckPatinetExist : IRequest<string>
+    {
+        public string userName { get; set; }
+        public string password { get; set; }
+    }
+
+    public class CheckPatinetExistHandler : IRequestHandler<CheckPatinetExist, string>
+    {
+        private readonly AppointmentSchedulingContext context;
+
+        public CheckPatinetExistHandler(AppointmentSchedulingContext context)
+        {
+            this.context = context;
+        }
+        async Task<string> IRequestHandler<CheckPatinetExist, string>.Handle(CheckPatinetExist request, CancellationToken cancellationToken)
+        {
+            Boolean exist = context.patients.Any(x => x.UserName == request.userName && x.Password == request.password);
+            if (exist)
+            {
+                return await Task.FromResult("Correct");
+            }
+            else
+            {
+                return await Task.FromResult("InCorrect username or password ");
+            }
+
+        }
+    }
+}
