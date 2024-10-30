cda25e28715992773e9ab8e598c95f0c45aeea9e
Asna A <asna.a@geekywolf.local>
2024-10-30 17:36:56 +0530

    specializationpage

--- AppointmentProject/src/app/app.routes.ts
+++ AppointmentProject/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { PatientLoginComponent } from './patient-login/patient-login.component';
 import { PatientProfileComponent } from './patient-profile/patient-profile.component';
 import { LandingComponent } from './landing/landing.component';
 import { ProfilePageComponent } from './profile-page-patinet/profile-page.component';
+import { SpecializationsComponent } from './specializations/specializations.component';
 
 export const routes: Routes = [
     {path:'patientSignup',component:PatientSignupComponent},
@@ -11,5 +12,7 @@ export const routes: Routes = [
     {path:'patientProfile',component:PatientProfileComponent},
     {path:'home',component:LandingComponent},
     {path:'patient-profile',component:ProfilePageComponent},
+    {path:'specializations',component:SpecializationsComponent},
     {path:'',redirectTo:'/home',pathMatch:'full'}
+
 ];
---
+++ AppointmentScheduling.Application/Request/Queries/CountDoctorQuery.cs
@@ -0,0 +1,32 @@
+﻿using AppointmentScheduling.Application.DTO;
+using AppointmentScheduling.Domain.Entity;
+using AppointmentScheduling.Infrastructure.Data;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+namespace AppointmentScheduling.Application.Request.Commands
+{
+    public class CountDoctorQuery : IRequest<GetCountDTO>
+    {
+        public int Id { get; set; }
+    }
+
+    public class CountDoctorHandler : IRequestHandler<CountDoctorQuery, GetCountDTO>
+    {
+        private readonly AppointmentSchedulingContext _context;
+
+        public CountDoctorHandler(AppointmentSchedulingContext context)
+        {
+            _context = context;
+        }
+        public async Task<GetCountDTO> Handle(CountDoctorQuery request, CancellationToken cancellationToken)
+        {
+            
+                GetCountDTO getCountDTO=new GetCountDTO();
+                var count = await _context.DoctorSpecializations.CountAsync(x=>x.SpecializationId==request.Id);
+
+                return new GetCountDTO { Count = count };
+
+
+        }
+    }
+}
