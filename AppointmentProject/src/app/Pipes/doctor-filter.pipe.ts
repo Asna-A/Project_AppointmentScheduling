b7b2873e0dcb433363a62e2d3913d51c1a2bb4c4
mukhil geeky <mukhil.mp@geekywolf.com>
2024-11-06 11:31:08 +0530

    pipeEdit

--- AppointmentProject/src/app/Pipes/doctor-filter.pipe.ts
+++ AppointmentProject/src/app/Pipes/doctor-filter.pipe.ts
@@ -8,8 +8,14 @@ export class DoctorFilterPipe implements PipeTransform {
 
   transform(doctors: any[],doctorName : string): any {
    const searchTerm = doctorName.toLowerCase();
+   if(doctorName!==''){
     return doctors.filter(doctor => doctor.name.toLowerCase().includes(searchTerm))
 
     }
+    else if(doctorName===''){
+      return [];
+
+    }
   }
+}
 
